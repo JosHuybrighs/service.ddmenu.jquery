2abe6ddfa44b53fdbd8a0cc508e8678de5043990
Jos Huybrighs <jhuybrighs@hotmail.com>
2014-11-15 18:55:28 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,9 @@
+##service.ddmenu.jquery - Single Level Dropdown Menu
+
+###Description
+
+`service.ddmenu.jquery` is an extremely lightweight (less than 800 bytes minified) jquery component that makes it easy to create individual, single-level dropdown menus on a web page. The dropdown menu itself must be fully designed in HTML (as per an imposed container layout). The plugin does't insert nor replaces elements in the DOM.
+
+###Tutorial and Documentation
+
+You can find the tutorial and API description at [code.cwwonline.be](http://code.cwwonline.be/serviceddmenujquery).
---
+++ src/service.ddmenu.jquery.js
@@ -0,0 +1,47 @@
+﻿; (function ($, win, undefined) {
+
+    $.DDMenu = function(element, options) {
+        this.settings = {
+            width: 0
+        };
+        this.dd = $(element);
+        this._init(options);
+    };
+
+    $.DDMenu.prototype = {
+        _init: function (options) {
+            this.settings = $.extend({}, this.settings, options);
+            var self = this;
+
+            // Setup styles
+            this.dd.addClass('ddMenu');
+            var ddList = this.dd.find('> ul');
+            ddList.addClass('ddMenuDropDown');
+            if (this.settings.width != 0) {
+                ddList.css({ width: this.settings.width });
+            }
+
+            // Bind event handlers
+            this.dd.on('click.ddMenu', function (event) {
+                // Close possible other open dropdowns
+                var otherOpenObjs = $('.ddMenuOpen').not(this.dd);
+                otherOpenObjs.removeClass('ddMenuOpen');
+
+                self.dd.toggleClass('ddMenuOpen');
+                event.stopPropagation();
+            });
+            $('html').on('click.ddMenu', function () {
+                self.dd.removeClass('ddMenuOpen');
+            });
+        }
+    };
+
+    $.fn.ddmenu = function (options) {
+        return this.each(function () {
+            if ($(this).data('ddmenu-plugin')) return;
+            var plugin = new $.DDMenu(this, options);
+            $(this).data('ddmenu-plugin', plugin);
+        });
+    };
+
+})(jQuery);
